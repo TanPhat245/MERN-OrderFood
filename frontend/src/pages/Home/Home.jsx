989c5731319c5c09c5c17288a660bd1bae565480
PhatCPU <145765612+TanPhat245@users.noreply.github.com>
2024-10-11 15:09:57 +0700

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import './Home.css'
-import Header from '../../components/Header/Header'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/AppDownload/AppDownload'
-import InstructOrder from '../../components/InstructOrder/InstructOrder'
-import Detail from '../../components/Detail/Detail'
+import Header from '../../components/Header/Header.jsx'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.jsx'
+import AppDownload from '../../components/AppDownload/AppDownload.jsx'
+import InstructOrder from '../../components/InstructOrder/InstructOrder.jsx'
+import Detail from '../../components/Detail/Detail.jsx'
 
 const Home = () => {
 
